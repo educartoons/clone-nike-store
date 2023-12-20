4d0a5030063aa6b6cfe3976ca43c7f1517c14eb6
Eduar Apaza <eapaza@learnsoft.com>
2023-12-19 22:14:17 -0500

    config node as variable

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -5,18 +5,30 @@ import { firestore } from "../../firebase/index";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
 
   const fetchProducts = async () => {
     const collectionRef = firestore.collection("products");
-    const snapshot = await collectionRef.get();
 
-    const fetchedData = snapshot.docs.map((doc) => {
-      return {
-        id: doc.id,
-        ...doc.data(),
-      };
-    });
-    setProducts(fetchedData);
+    try {
+      const snapshot = await collectionRef.get();
+
+      const fetchedData = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      if (fetchedData.length === 0) {
+        setError(true);
+      } else {
+        setProducts(fetchedData);
+      }
+    } catch (err) {
+      console.log("esto es error");
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -28,6 +40,11 @@ export default function Products() {
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
+      {error && (
+        <h2 className="col-span-3 text-center text-5xl mt-10">
+          Oops, something went wrong
+        </h2>
+      )}
     </div>
   );
 }
